4105519b638fb98f6f5edf2e04121cd216056e41
Nausicaa <softwareupdate1.0@mail.ru>
2026-05-07 13:30:22 +0300

    fuck you

---
+++ heygit.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CourseWork;
+
+public partial class heygit : Window
+{
+    public heygit()
+    {
+        InitializeComponent();
+    }
+}
